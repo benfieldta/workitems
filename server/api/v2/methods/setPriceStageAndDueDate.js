11d0ce16e91fc0950ca6d35baa758b500e394b19
Juan Carlos Gonzalez <jcgonzalezmolina@gmail.com>
2018-02-27 14:10:39 -0500

    refactored code so that workitems are fetched from db in one call instead of making one call for each workitem

--- server/api/v2/methods/setPriceStageAndDueDate.js
+++ server/api/v2/methods/setPriceStageAndDueDate.js
@@ -1,15 +1,24 @@
 const moment = require('moment');
 const { Workitem } = require('../../../db/schemas/Workitem');
 
-module.exports = async function(issue, repo) {
-  let issue_number = issue.number.toString();
-  // var data = await getData(issue_number, repo);
-  console.log(`grabbing extra data for issue number ${issue_number}`);
-  var data = await Promise.resolve(Workitem.findOne({$and: [{itemId: issue_number},{repo: repo}]}).lean());
-  // console.log(data);
-  issue.price = data.price;
-  issue.stage = data.stage;
-  issue.assignee = data.assignee;
-  issue.due_date = moment(data.dueDate).format("MMMM Do, YYYY");
-  return issue;
+module.exports = async function(issues, repo) {
+  var data = await Workitem.find({repo: repo}).lean();
+  issues = issues.map(issue => {
+    let issue_number = issue.number.toString();
+    let workitem = data.find(wi => wi.itemId === issue_number);
+    issue.price = workitem.price;
+    issue.stage = workitem.stage;
+    issue.assignee = workitem.assignee;
+    issue.due_date = moment(workitem.dueDate).format("MMMM Do, YYYY");
+    return issue;
+  });
+  return issues;
 }
+
+// let issue_number = issue.number.toString();
+//   var data = await Promise.resolve(Workitem.findOne({$and: [{itemId: issue_number},{repo: repo}]}).lean());
+//   issue.price = data.price;
+//   issue.stage = data.stage;
+//   issue.assignee = data.assignee;
+//   issue.due_date = moment(data.dueDate).format("MMMM Do, YYYY");
+//   return issue;
