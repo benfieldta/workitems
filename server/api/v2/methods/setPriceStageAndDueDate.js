6bba5cadccda1f72a820e554512f75edd9d11d1e
Juan Carlos Gonzalez <jcgonzalezmolina@gmail.com>
2018-02-27 13:09:45 -0500

    condensing code

--- server/api/v2/methods/setPriceStageAndDueDate.js
+++ server/api/v2/methods/setPriceStageAndDueDate.js
@@ -1,10 +1,15 @@
-var getData = require('./getPriceStageAndDueDate');
+const moment = require('moment');
+const { Workitem } = require('../../../db/schemas/Workitem');
+
 module.exports = async function(issue, repo) {
   let issue_number = issue.number.toString();
-  var data = await getData(issue_number, repo);
+  // var data = await getData(issue_number, repo);
+  console.log(`grabbing extra data for issue number ${issue_number}`);
+  var data = await Promise.resolve(Workitem.findOne({$and: [{itemId: issue_number},{repo: repo}]}).lean());
+  // console.log(data);
   issue.price = data.price;
   issue.stage = data.stage;
   issue.assignee = data.assignee;
-  issue.due_date = data.due_date;
+  issue.due_date = moment(data.dueDate).format("MMMM Do, YYYY");
   return issue;
 }
